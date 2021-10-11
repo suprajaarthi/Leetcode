bf20faab6a302411b1c39e3bc9ba7ec2ee59aa9c
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-10-11 14:33:44 +0530

    Create Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] < target:
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                return mid
+        return -1
+
+      
+      
+# Example 1:
+
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Explanation: 9 exists in nums and its index is 4
+# Example 2:
+
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Explanation: 2 does not exist in nums so return -1
