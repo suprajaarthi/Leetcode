d2543aa6271bbe365a92ffeb2791fb63fab4fff6
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-11-08 14:47:19 +0530

    BST - 1

--- IndexSearch.py
+++ IndexSearch.py
@@ -14,7 +14,7 @@ class Solution:
         return left
     
     
-    """
+"""
 You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. 
 Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
@@ -22,8 +22,6 @@ Suppose you have n versions [1, 2, ..., n] and you want to find out the first ba
 
 You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version.
 You should minimize the number of calls to the API.
-
- 
 
 Example 1:
 
